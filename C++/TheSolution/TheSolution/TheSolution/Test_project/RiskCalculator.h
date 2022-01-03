74c1512a6b503290da295596bdf15727cda983df
Antoine <antoine.marion@usherbrooke.ca>
2022-01-03 14:35:55 -0500

    C++ project beginning

---
+++ C++/TheSolution/TheSolution/TheSolution/Test_project/RiskCalculator.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/*
+* This is the risk formula considering the state
+* of persons in the simulation overtime
+* 
+* Start with Microsoft tutorial on calculator
+* https://docs.microsoft.com/fr-fr/cpp/get-started/tutorial-console-cpp?view=msvc-160
+*/
+class RiskCalculator
+{
+};
+
